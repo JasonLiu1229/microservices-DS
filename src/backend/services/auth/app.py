8ce6fbb94a8cdad2f386319861a2b7b83fb76c14
Jason Liu <Liujason2003@gmail.com>
2024-04-27 19:18:58 +0200

    some fixes

--- src/backend/services/auth/app.py
+++ src/backend/services/auth/app.py
@@ -3,9 +3,11 @@ FastAPI application for the auth service.
 """
 import auth
 import healthcheck
+import users
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(healthcheck.router, prefix="/health", tags=["health_check"])
+app.include_router(users.router, prefix="/users", tags=["users"])
---
+++ src/backend/services/auth/users.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Response
+from wrapper import AuthWrapper
+
+router = APIRouter(responses={404: {"description": "Not found"}})
+
+@router.get("")
+async def get_users() -> Response:
+    """Get all users.
+
+    Returns:
+        Response: 200 OK
+    """
+    auth_wrapper = AuthWrapper()
+    users = auth_wrapper.get_users()
+    users_username = [user.username for user in users]
+    return Response(content={"users": users_username}, status_code=200)
